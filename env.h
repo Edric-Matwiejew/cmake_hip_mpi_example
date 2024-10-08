2fa144184750ec0a54c956188ead7bf8b56723cb
Edric-Matwiejew <edric.matviev@example.com>
2024-10-08 14:54:11 +0800

    type fix in gcc version

--- env.h
+++ env.h
@@ -2,6 +2,6 @@ module load cmake/3.27.7
 module load cray-mpich/8.1.27
 module load rocm/5.7.1
 module load hipfort/5.7.1
-module load gcc/12.0.2
+module load gcc/12.2.0
 
 export LD_LIBRARY_PATH=$PWD/../lib:$LD_LIBRARY_PATH
